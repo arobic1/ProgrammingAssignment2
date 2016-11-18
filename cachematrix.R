45f35f770ad6c84066ef15c9d4c36a04f343b047
arobic <arobic@gmail.com>
2016-11-17 20:19:52 -0500

    Programming assignment #2 completed.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions to return the inverse of a matrix from cache 
+## if it's already been computed
+## To use:
+## - Create an invertible nxn matrix called my_matrix
+## - save makeCacheMatrix(my_matrix) into my_list
+## - use cacheSolve(my_list) to get the inverse 
+##    (first time computed, following times from cache)
 
-## Write a short comment describing this function
+## makeCacheMatrix create a list of functions to save and retrieve the result
+## of the matrix inversion computation
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) m <<- inv
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve performs the computation and retrieval from cache
+## using the functions defined by makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
 }
